8fa1677638f4f950c5999d04bab44f6f8f637d37
LuisMikey22 <llucatero_23@alu.uabcs.mx>
2025-03-25 00:08:57 -0700

    Diseño 2 del juego de rompecabezas 15 Puzzle

--- interfazGrafica/src/application/PuzzleNumerico.java
+++ interfazGrafica/src/application/PuzzleNumerico.java
@@ -2,18 +2,13 @@ package application;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
-import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -24,6 +19,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class PuzzleNumerico {
 
@@ -70,7 +66,7 @@ public class PuzzleNumerico {
 		frame.setIconImage(puzzleIcon.getImage());
 		
  		frame.setTitle("Rompecabezas numérico");
- 		frame.setSize(760, 680); 
+ 		frame.setSize(880, 680); 
  		frame.setResizable(false);
  		frame.setLocationRelativeTo(null);
  		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -82,7 +78,7 @@ public class PuzzleNumerico {
 		
 		JPanel panelTablero = new JPanel();
 		panelTablero.setSize(440, 440);
-		panelTablero.setLocation(77, 100);
+		panelTablero.setLocation(54, 100);
 		panelTablero.setBackground(new Color(31, 18, 5, 155));
 		panelTablero.setLayout(new GridLayout(4, 4, 8, 8));
 		panelFondo.add(panelTablero, BorderLayout.CENTER);
@@ -93,6 +89,8 @@ public class PuzzleNumerico {
 		tableroFondoImg.setLocation(200, 90);
 		panelFondo.add(tableroFondoImg);
 		
+		
+		//barra lateral
 		JPanel barraLateral = new JPanel();
 		barraLateral.setBackground(Color.black);
 		barraLateral.setLayout(new GridBagLayout());;
@@ -100,19 +98,75 @@ public class PuzzleNumerico {
 		c.fill = GridBagConstraints.HORIZONTAL; //horizontal
 		panelFondo.add(barraLateral, BorderLayout.EAST);
 		
-		JButton reiniciar = new JButton("Reiniciar");
-		reiniciar.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.decode("#D8A069")));
-		reiniciar.setBackground(Color.decode("#3F2B18"));
-		reiniciar.setForeground(Color.decode("#FFD09E"));
-		reiniciar.setFocusPainted(false); //hace invisible el recuadro blanco al presionar el botón
-		reiniciar.setFont(new Font("Kefe", Font.BOLD, 20)); //fuente, tipo y tamaño
+		
+		//contador
+		JPanel contadorPnl = new JPanel();
+		contadorPnl.setOpaque(true);
+		contadorPnl.setBackground(Color.decode("#FFD09E"));
+		contadorPnl.setLayout(new GridLayout(1, 2));
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(0, 0, 0, 20);  //relleno en la parte de arriba
 		c.gridx = 0;
 		c.gridy = 0;
 		c.ipady = 20;
-		c.ipadx = 40;
-		barraLateral.add(reiniciar, c);
+		barraLateral.add(contadorPnl, c);
+		
+		JLabel contadorLbl = new JLabel("Contador: ");
+		contadorLbl.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 0, Color.decode("#3F2B18")));
+		contadorLbl.setForeground(Color.decode("#3F2B18"));
+		contadorLbl.setFont(new Font("Kefe", Font.BOLD, 20)); //fuente, tipo y tamaño
+		contadorLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		contadorPnl.add(contadorLbl);
+		
+		JLabel contadorLblNum = new JLabel("0");
+		contadorLblNum.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.decode("#3F2B18")));
+		contadorLblNum.setForeground(Color.decode("#3F2B18"));
+		contadorLblNum.setFont(new Font("Kefe", Font.BOLD, 16)); //fuente, tipo y tamaño
+		contadorLblNum.setHorizontalAlignment(SwingConstants.CENTER);
+		contadorPnl.add(contadorLblNum);
+		
+		
+		//temporizador
+		JPanel temporizadorPnl = new JPanel();
+		temporizadorPnl.setOpaque(true);
+		temporizadorPnl.setBackground(Color.decode("#FFD09E"));
+		temporizadorPnl.setLayout(new GridLayout(1, 2));
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10, 0, 0, 20);  //relleno en la parte de arriba
+		c.gridx = 0;
+		c.gridy = 1;
+		c.ipady = 20;
+		barraLateral.add(temporizadorPnl, c);
+		
+		JButton temporizadorBtn = new JButton("Temporizador: ");
+		temporizadorBtn.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 0, Color.decode("#3F2B18")));
+		temporizadorBtn.setBackground(Color.decode("#3F2B18"));
+		temporizadorBtn.setForeground(Color.decode("#FFD09E"));
+		temporizadorBtn.setFocusPainted(false); //hace invisible el recuadro blanco al presionar el botón
+		temporizadorBtn.setFont(new Font("Kefe", Font.BOLD, 20)); //fuente, tipo y tamaño
+		temporizadorPnl.add(temporizadorBtn);
+		
+		JLabel temporizadorLblNum = new JLabel("0"+"s");
+		temporizadorLblNum.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.decode("#3F2B18")));
+		temporizadorLblNum.setForeground(Color.decode("#3F2B18"));
+		temporizadorLblNum.setFont(new Font("Kefe", Font.BOLD, 16)); //fuente, tipo y tamaño
+		temporizadorLblNum.setHorizontalAlignment(SwingConstants.CENTER);
+		temporizadorPnl.add(temporizadorLblNum);
+		
+		
+		//botón reiniciar
+		JButton reiniciarBtn = new JButton("Reiniciar");
+		reiniciarBtn.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.decode("#3F2B18")));
+		reiniciarBtn.setBackground(Color.decode("#3F2B18"));
+		reiniciarBtn.setForeground(Color.decode("#FFD09E"));
+		reiniciarBtn.setFocusPainted(false); //hace invisible el recuadro blanco al presionar el botón
+		reiniciarBtn.setFont(new Font("Kefe", Font.BOLD, 20)); //fuente, tipo y tamaño
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10, 0, 0, 20);  //relleno en la parte de arriba
+		c.gridx = 0;
+		c.gridy = 2;
+		c.ipady = 20;
+		barraLateral.add(reiniciarBtn, c);
 		
 		
 		//crear la matriz de botones
@@ -125,9 +179,10 @@ public class PuzzleNumerico {
  				if(y==0 && x==0) { //dejar el primer espacio vacío
  					matrizBotones[y][x] = new JButton(" ");
  				}else {
- 					matrizBotones[y][x] = new JButton(numeros[numPos]);
- 					numPos++;
+ 					matrizBotones[y][x] = new JButton(numeros[numPos-1]);
  				}
+ 				numPos++;
+ 				System.out.println("numPos: " + numPos);
  				
  				matrizBotones[y][x].setBackground(Color.decode("#FFD09E"));
  				matrizBotones[y][x].setForeground(Color.decode("#40250A"));
